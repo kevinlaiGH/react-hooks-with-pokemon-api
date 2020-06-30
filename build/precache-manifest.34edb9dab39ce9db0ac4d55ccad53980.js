d8ad84cd121d798df3251753eef97362b966d864
Kevin Lai <kevin.km.lai@gmail.com>
2020-06-30 20:28:44 +1000

    upload build version

---
+++ build/precache-manifest.34edb9dab39ce9db0ac4d55ccad53980.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745f6f4f332bc056821c4efa419e3517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c852dc48b73a30ad5ee4",
+    "url": "/static/css/2.1b935ff3.chunk.css"
+  },
+  {
+    "revision": "0ac6116f8f8b296db371",
+    "url": "/static/css/main.ae934c4d.chunk.css"
+  },
+  {
+    "revision": "c852dc48b73a30ad5ee4",
+    "url": "/static/js/2.34bf5870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34bf5870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac6116f8f8b296db371",
+    "url": "/static/js/main.6b3adc59.chunk.js"
+  },
+  {
+    "revision": "ceab470f849bc2306c0f",
+    "url": "/static/js/runtime-main.5832c6a8.js"
+  }
+]);
